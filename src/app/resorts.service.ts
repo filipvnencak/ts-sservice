06684a3c2f60fcea7d994e70b4bf2288c86d6e89
Filip Vnenčák <vnencak.filip@gmail.com>
2021-06-01 16:50:00 +0200

    Colors, logos

--- src/app/resorts.service.ts
+++ src/app/resorts.service.ts
@@ -6,14 +6,20 @@ import { Injectable } from '@angular/core';
 export class ResortsService {
   private Resosrts: any [] =
 [
-  {id: 1,name: 'Bachledová Dolina', img:'https://drive.google.com/file/d/1RDz4q2G7VHCP4NoocU1ZNoORyXU2gNXj/view?usp=sharing'},  
-  {id: 2,name: 'Bania Ski&Fun', img: 'http://www.img.1pk0.sk/logo.png'}, 
-  {id: 3,name: 'CzorstynSki', img: 'http://www.img.1pk0.sk/logo.png'},  
-  {id: 4,name: 'Kotelnica', img: 'http://www.img.1pk0.sk/logo.png'},
-  {id: 5,name: 'GrapaSki', img: 'http://www.img.1pk0.sk/logo.png'},  
-  {id: 6,name: 'Harenda', img: 'http://www.img.1pk0.sk/logo.png'},  
-  {id: 7,name: 'Harenda', img: 'http://www.img.1pk0.sk/logo.png'},
-  {id: 8,name: 'Bachledová Dolina', img: 'http://www.img.1pk0.sk/logo.png'}
+  {id: 1,name: 'Bachledová Dolina', logo:'bachledova_ski'},  
+  {id: 2,name: 'Bania Ski&Fun', logo: 'bania_ski_fun'}, 
+  {id: 3,name: 'CzorstynSki', logo: 'czorsztyn_ski'},  
+  {id: 4,name: 'GrapaSki', logo: 'grapaski'},
+  {id: 5,name: 'Harenda', logo: 'harenda'},  
+  {id: 6,name: 'Jurogow Ski', logo: 'jurgow_ski'},  
+  {id: 7,name: 'Kaniowka', logo: 'kaniowka'},
+  {id: 8,name: 'Kotelnica Bialczanska', logo: 'kotelnica_bialczanska'},
+  {id: 9,name: 'Koziniec Ski', logo: 'koziniec_ski'},
+  {id: 10,name: 'Mosorny Gron', logo: 'pkl_mosorny_gron'},
+  {id: 11,name: 'Palenica', logo: 'pkl_palenica_2'},
+  {id: 12,name: 'Polana Szymoszkowa', logo: 'polana_szymoszkowa'},
+  {id: 13,name: 'Suche', logo: 'suche'},
+  {id: 14,name: 'Witow Ski', logo: 'witow_ski'},
 ]
 
   constructor() { 
