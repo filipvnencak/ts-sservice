91e4a12fbdfa6a2af8b802d389e646301fbe3d0a
Filip Vnenčák <vnencak.filip@gmail.com>
2021-06-03 09:19:38 +0200

    Cover, weather html

--- src/app/resorts.service.ts
+++ src/app/resorts.service.ts
@@ -6,20 +6,121 @@ import { Injectable } from '@angular/core';
 export class ResortsService {
   private Resosrts: any [] =
 [
-  {id: 1,name: 'Bachledová Dolina', logo:'bachledova_ski'},  
-  {id: 2,name: 'Bania Ski&Fun', logo: 'bania_ski_fun'}, 
-  {id: 3,name: 'CzorstynSki', logo: 'czorsztyn_ski'},  
-  {id: 4,name: 'GrapaSki', logo: 'grapaski'},
-  {id: 5,name: 'Harenda', logo: 'harenda'},  
-  {id: 6,name: 'Jurogow Ski', logo: 'jurgow_ski'},  
-  {id: 7,name: 'Kaniowka', logo: 'kaniowka'},
-  {id: 8,name: 'Kotelnica Bialczanska', logo: 'kotelnica_bialczanska'},
-  {id: 9,name: 'Koziniec Ski', logo: 'koziniec_ski'},
-  {id: 10,name: 'Mosorny Gron', logo: 'pkl_mosorny_gron'},
-  {id: 11,name: 'Palenica', logo: 'pkl_palenica_2'},
-  {id: 12,name: 'Polana Szymoszkowa', logo: 'polana_szymoszkowa'},
-  {id: 13,name: 'Suche', logo: 'suche'},
-  {id: 14,name: 'Witow Ski', logo: 'witow_ski'},
+  { 
+    id: 1,
+    name: 'Bachledová Dolina', 
+    logo:'bachledova_ski', 
+    cover: 'bachledova'
+  },  
+  { 
+    id: 2,
+    name: 'Bania Ski&Fun', 
+    logo: 'bania_ski_fun', 
+    cover: 'Bania1'
+  }, 
+  {
+    id: 3,
+    name: 'CzorstynSki', 
+    logo: 'czorsztyn_ski', 
+    cover: 'CzorsztynSki1'
+  }, 
+  {
+    id: 4,
+    name: 'Długa Polana', 
+    logo: 'dluga_polana', 
+    cover: 'DlugaPolana'
+  }, 
+  {
+    id: 5,
+    name: 'GrapaSki', 
+    logo: 'grapaski', 
+    cover: 'GrapaSki1'
+  },
+  {
+    id: 6,
+    name: 'Harenda', 
+    logo: 'harenda', 
+    cover: 'Harenda'
+  }, 
+  {
+    id: 7,
+    name: 'Horników Wierch', 
+    logo: 'hornikow_kolor', 
+    cover: 'Hornikow'
+  },   
+  {
+    id: 8,
+    name: 'Jurogow Ski', 
+    logo: 'jurgow_ski', 
+    cover: 'JurgowSki'
+  },  
+  {
+    id: 9,
+    name: 'Kaniowka', 
+    logo: 'kaniowka',
+    cover: 'kaniowka'
+
+  },
+  {
+    id: 10,
+    name: 'Kotelnica Białaczańska', 
+    logo: 'kotelnica_bialczanska',
+    cover: 'Kotelnica'
+  },
+  {
+    id: 11,
+    name: 'Koziniec Ski', 
+    logo: 'koziniec_ski',
+    cover: 'Koziniec'
+  },
+  {
+    id: 12,
+    name: 'Meander Oravice', 
+    logo: 'meander',
+    cover: 'Meander'
+  },
+  {
+    id: 13,
+    name: 'Mosorny Groń', 
+    logo: 'pkl_mosorny_gron',
+    cover: 'PKL_MOSORNY_GROŃ'
+  },
+  {
+    id: 14,
+    name: 'Palenica', 
+    logo: 'pkl_palenica_2',
+    cover: 'PKLPalenica1'
+  },
+  {
+    id: 15,
+    name: 'Polana Szymoszkowa', 
+    logo: 'polana_szymoszkowa',
+    cover: 'Polana_szymoszkowa'
+  },
+  {
+    id: 16,
+    name: 'Rusiński', 
+    logo: 'rusinski',
+    cover: 'RusinSki'
+  },
+  {
+    id: 17,
+    name: 'Strachan Ski', 
+    logo: 'Logo_Ski Centrum Strachan',
+    cover: 'Strachan'
+  },
+  {
+    id: 18,
+    name: 'Suche', 
+    logo: 'suche',
+    cover: 'Suche'
+  },
+  {
+    id: 19,
+    name: 'Witow Ski', 
+    logo: 'witow_ski',
+    cover: 'WitówSki1'
+  },
 ]
 
   constructor() { 
